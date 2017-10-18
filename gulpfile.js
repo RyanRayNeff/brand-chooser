e096a7e2d3373a9fff7490b7ff77e67be5524046
RyanNeff <ryan.ray.neff@fluke.com>
2017-10-18 16:11:44 -0700

    switched off minification, switched on file copying and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -38,48 +38,48 @@ gulp.task('styles', function(){
     .pipe(inject(injectGlobalFiles, injectGlobalOptions))
     .pipe(inject(injectAppFiles, injectAppOptions))
     .pipe(sass())
-    .pipe(uncss({
-            html: ['src/*.html'],
-            ignore : [  // These support basic bootstrap functions
-                        /\.affix/,
-                        // /\.alert/,
-                        /\.close/,
-                        /\.collapse/,
-                        /\.fade/,
-                        /\.has/,
-                        // /\.help/,
-                        /\.in/,
-                        /\.modal/,
-                        /\.open/,
-                        // /\.popover/,
-                        // /\.tooltip/,
-                        // These support tab-collapse.js
-                        /\.visible-xs/,
-                        /\.hidden-xs/,
-                        /.panel.*/
-                        ],
-            }))
+    // .pipe(uncss({
+    //         html: ['src/*.html'],
+    //         ignore : [  // These support basic bootstrap functions
+    //                     /\.affix/,
+    //                     // /\.alert/,
+    //                     /\.close/,
+    //                     /\.collapse/,
+    //                     /\.fade/,
+    //                     /\.has/,
+    //                     // /\.help/,
+    //                     /\.in/,
+    //                     /\.modal/,
+    //                     /\.open/,
+    //                     // /\.popover/,
+    //                     // /\.tooltip/,
+    //                     // These support tab-collapse.js
+    //                     /\.visible-xs/,
+    //                     /\.hidden-xs/,
+    //                     /.panel.*/
+    //                     ],
+    //         }))
     .pipe(autoprefixer('last 5 versions', 'ie >= 8'))
     // When inline injection is turned on, you'll want to switcht this back on
-    .pipe(sourcemaps.write('src/compiled-css'))
-    .pipe(gulp.dest('src/compiled-css'));
-    // .pipe(gulp.dest('dist/styles'));
+    .pipe(sourcemaps.write())
+    // .pipe(gulp.dest('src/compiled-css'));
+    .pipe(gulp.dest('dist/styles'));
 });
 
-gulp.task('html', ['styles'], function(){
+gulp.task('html', ['styles', 'copy-images', 'copy-favicons', 'copy-javascript'], function(){
  
   return gulp.src('src/index.html')
     // This inlines the css
-    .pipe(inlinesource())
+    //.pipe(inlinesource())
     
     // This minifies html output. 
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      minifyCSS: true,
-      minifyJS: true,
-      //removeComments: true,
-      useShortDoctype: true
-    }))
+    // .pipe(htmlmin({
+    //   collapseWhitespace: true,
+    //   minifyCSS: true,
+    //   minifyJS: true,
+    //   //removeComments: true,
+    //   useShortDoctype: true
+    // }))
 
     .pipe(gulp.dest('dist'))
     .pipe(notify({
@@ -101,4 +101,22 @@ gulp.task('default', function () {
   });
   gulp.watch('src/styles/*.scss', ['sass-watch']);
   gulp.watch('src/*.html', ['html-watch']);
+});
+
+//copies images from sourc to dist
+gulp.task('copy-images', function () {
+    gulp.src('src/assets/images/*')
+        .pipe(gulp.dest('dist/images'));
+});
+
+//copies favicons from sourc to dist
+gulp.task('copy-favicons', function () {
+    gulp.src('src/assets/favicons/*')
+        .pipe(gulp.dest('dist'));
+});
+
+//copies js from sourc to dist
+gulp.task('copy-javascript', function () {
+    gulp.src('src/assets/javascript/*')
+        .pipe(gulp.dest('dist/js'));
 });
